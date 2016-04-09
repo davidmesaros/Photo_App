efad29645197b0997a65350f1b9db2e8254ac093
davidmesaros <david.mesaros@hotmail.com>
2016-04-09 23:46:29 +1000

    add devise with g - its the best

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user! #access the app you need to be user that's logged.
 end

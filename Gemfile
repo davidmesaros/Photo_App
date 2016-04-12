723ded0e9833671163c23d49115c8e3e3e87af87
davidmesaros <david.mesaros@hotmail.com>
2016-04-12 22:14:26 +1000

    add strip and intregate with devise sign in

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '4.2.2'
 gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
+gem 'stripe'
+gem 'dotenv-rails', :groups => [:development, :test]
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 # Use SCSS for stylesheets
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
+  accepts_nested_attributes_for :payment # sign up user in the new form is saved as with the payment confrirmation is save in one hit eg after valid credit card is added.
 end
